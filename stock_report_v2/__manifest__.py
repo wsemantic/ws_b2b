bf2f62291397d78f80de6f2d5a80858731385c0b
Alfonso Gonzalez <info@wsemantic.com>
2025-05-26 18:12:55 +0200

    demo data - server action

--- stock_report_v2/__manifest__.py
+++ stock_report_v2/__manifest__.py
@@ -18,6 +18,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/stock_report_config_views.xml',
+        'data/demo_server_action.xml'
         # 'views/menu_views.xml',
     ],
     'assets': {
